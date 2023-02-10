ad5e79fcc5eb5e2731cdade950d30b99b16dbb6e
Saravanesh <raja4redbox@gmail.com>
2023-02-10 19:09:16 +0530

    Create returner.py

---
+++ returner.py
@@ -0,0 +1,10 @@
+import sys
+
+# Take input value from the command line
+input_value = int(sys.argv[1])
+
+# Perform calculation on input value
+result = input_value ** 2
+
+# Return result
+print(result)
