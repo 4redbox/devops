3c1305ee1f7addc4a88d75ba28e463bf9ea57384
Saravanesh <raja4redbox@gmail.com>
2023-02-10 19:32:50 +0530

    Update returner.py

--- returner.py
+++ returner.py
@@ -2,13 +2,11 @@
 
 import sys
 
-# Take input value from the command line
-input_value = int(sys.argv[1])
-
 # Check if an argument was passed in
 if len(sys.argv) < 2:
     print("Error: No argument passed in")
 else:
 # Perform calculation on input value
+  input_value = int(sys.argv[1])
   result = input_value ** 2
   print(result)
